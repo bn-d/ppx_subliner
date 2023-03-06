f16ec6bf0f277911a6f8e5122349f7d32f343101
Boning <me@boni.ng>
2023-03-06 20:03:31 +0000

    Support prefix

--- src/attribute_utils.ml
+++ src/attribute_utils.ml
@@ -15,37 +15,44 @@ let get_expr name (attrs : attributes) =
            None)
 
 module Cmd_info = struct
-  let is_relevant = function
+  let to_args_label = function
     (* TODO: deprecated_ support *)
-    | "version" | "deprecated" | "docs" | "sdocs" | "exits" | "envs" | "man"
-    | "man_xrefs" ->
-        true
+    | "version" | "subliner.version" -> Some "version"
+    | "deprecated" | "subliner.deprecated" | "deprecated_" -> Some "deprecated"
+    | "docs" | "subliner.docs" -> Some "docs"
+    | "sdocs" | "subliner.sdocs" -> Some "sdocs"
+    | "exits" | "subliner.exits" -> Some "exits"
+    | "envs" | "subliner.envs" -> Some "envs"
+    | "man" | "subliner.man" -> Some "man"
+    | "man_xrefs" | "subliner.man_xrefs" -> Some "man_xrefs"
     (* name and doc will be handled separately *)
-    | "name" | "doc" | "ocaml.doc" | _ -> false
+    | "name" | "doc" | "ocaml.doc" | _ -> None
 
   let to_args ~(default_name_expr : expression) (attrs : attributes) :
       (arg_label * expression) list =
     (* get arguments that require special handling *)
     let name_arg =
       let expr =
-        get_expr "name" attrs |> Option.value ~default:default_name_expr
+        get_expr "subliner.name" attrs
+        |> (function Some e -> Some e | None -> get_expr "name" attrs)
+        |> Option.value ~default:default_name_expr
       in
       [ (Nolabel, expr) ]
     and doc_arg =
-      get_expr "doc" attrs
+      get_expr "subliner.doc" attrs
+      |> (function Some e -> Some e | None -> get_expr "doc" attrs)
       |> (function Some e -> Some e | None -> get_expr "ocaml.doc" attrs)
       |> function Some e -> [ (Labelled "doc", e) ] | None -> []
     in
     List.filter_map
       (fun (attr : attribute) ->
-        if is_relevant attr.attr_name.txt then
-          let loc = attr.attr_loc in
-          match attr.attr_payload with
-          | PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] ->
-              Some (Labelled attr.attr_name.txt, expr)
-          | _ -> payload_error ~loc
-        else
-          None)
+        to_args_label attr.attr_name.txt
+        |> Option.map (fun label ->
+               let loc = attr.attr_loc in
+               match attr.attr_payload with
+               | PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] ->
+                   (Labelled label, expr)
+               | _ -> payload_error ~loc))
       attrs
     @ doc_arg
     @ name_arg
@@ -55,7 +62,10 @@ module Default_term = struct
   let get : attributes -> expression option =
     Utils.list_find_map (fun (attr : attribute) ->
         let loc = attr.attr_loc in
-        if attr.attr_name.txt = "default" then
+        if
+          attr.attr_name.txt = "default"
+          || attr.attr_name.txt = "subliner.default"
+        then
           match attr.attr_payload with
           | PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] -> Some expr
           | _ -> payload_error ~loc
